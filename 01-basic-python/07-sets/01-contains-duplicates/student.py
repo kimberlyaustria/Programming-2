5988a168a4ae1cb44c0b242fb5d132fc974a9477
kimberlyaustria <kimberly.austria@student.ucll.be>
2023-02-25 16:13:19 +0100

    1.7 sets

--- 01-basic-python/07-sets/01-contains-duplicates/student.py
+++ 01-basic-python/07-sets/01-contains-duplicates/student.py
@@ -1,1 +1,5 @@
 # Write your code here
+def contains_duplicates(xs):
+    if len(xs) != len(set(xs)):
+        return True
+    return False
--- 01-basic-python/07-sets/02-remove-duplicates/student.py
+++ 01-basic-python/07-sets/02-remove-duplicates/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def remove_duplicates(xs):
+    empty_set = set()
+    result = []
+    for i in xs:
+        if i not in empty_set:
+            result.append(i)
+            empty_set.add(i)
+    
+    return result
